774c0e79324cb9c1c19a50e9042ae14a2ca0302f
Tyler Stephen <tyler.stephen@gmail.com>
2017-12-23 00:09:25 -0800

    Day 2, part 2

--- 02/main.go
+++ 02/main.go
@@ -14,7 +14,7 @@ func main() {
 	checksum := 0
 
 	for scanner.Scan() {
-		checksum += getRowDifference(scanner.Text())
+		checksum += getWholeDivisor(scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
@@ -41,4 +41,33 @@ func getRowDifference(s string) int {
 	}
 
 	return max - min
+}
+
+func getWholeDivisor(s string) int {
+	fmt.Println(s)
+
+	// We know there is exactly one pair of numbers what will divide without remainder.
+	// Do the n^2 approach until something more fancy comes along~
+
+	// Convert to ints now to make the double loop a bit nicer to deal with.
+	cells := strings.Split(s, "\t")
+	nums := make([]int, len(cells))
+	for i, cell := range cells {
+		nums[i], _ = strconv.Atoi(cell)
+	}
+
+	for i, x := range nums {
+		for _, y := range nums[i+1 : len(nums)] {
+			if x < y && y%x == 0 {
+				return y / x
+			}
+
+			if y < x && x%y == 0 {
+				return x / y
+			}
+		}
+	}
+
+	// the bad case :(
+	return -1
 }
