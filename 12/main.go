5713ad14beda92fcc2fa4635053b80c6dc26b790
Tyler Stephen <tyler.stephen@gmail.com>
2018-01-03 21:25:36 -0800

    Day 12, part 2

--- 12/main.go
+++ 12/main.go
@@ -7,10 +7,12 @@ import (
 	"strings"
 )
 
+type connectionMap map[string][]string
+
 func main() {
 
 	// 1. Make a big map of connections, (program -> connections)
-	unassignedPrograms := make(map[string][]string)
+	unassignedPrograms := make(connectionMap)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -22,33 +24,49 @@ func main() {
 		unassignedPrograms[program] = connections
 	}
 
-	// 2. Starting at 0, remove the element from the map of connections
-	// and then repeat this for each of the connections for the removed elements
+	// Part 2: To remove multiple groups, repeat the removal process
+	// from part 1 with a random unassigned program.
 
-	connectedPrograms := 0
+	groupCount := 0
+	for len(unassignedPrograms) > 0 {
+		groupCount++
 
-	programQueue := make(chan string, len(unassignedPrograms))
-	programQueue <- "0"
+		// 2. Starting at 0, remove the element from the map of connections
+		// and then repeat this for each of the connections for the removed elements
 
-	for p := range programQueue {
-		// Check if the program is unprocessed
-		if connections, found := unassignedPrograms[p]; found {
-			// Add connections for processing and remove from the map.
-			connectedPrograms++
-			delete(unassignedPrograms, p)
-			for _, c := range connections {
-				// Don't reprocess connections we've removed
-				if _, found := unassignedPrograms[c]; found {
-					programQueue <- c
+		connectedPrograms := 0
+		programQueue := make(chan string, len(unassignedPrograms))
+		programQueue <- unassignedPrograms.getKey()
+
+		for p := range programQueue {
+			// Check if the program is unprocessed
+			if connections, found := unassignedPrograms[p]; found {
+				// Add connections for processing and remove from the map.
+				connectedPrograms++
+				delete(unassignedPrograms, p)
+				for _, c := range connections {
+					// Don't reprocess connections we've removed
+					if _, found := unassignedPrograms[c]; found {
+						programQueue <- c
+					}
 				}
+			}
+
+			// Terminate the for loop when the channel is empty
+			if len(programQueue) == 0 {
+				close(programQueue)
 			}
 		}
 
-		// Terminate the for loop when the channel is empty
-		if len(programQueue) == 0 {
-			close(programQueue)
-		}
+		fmt.Printf("%02d: %d\n", groupCount, connectedPrograms)
+	}
+}
+
+func (cm connectionMap) getKey() string {
+	// Gives back the first key it finds, which is non-deterministic
+	for k := range cm {
+		return k
 	}
 
-	fmt.Println("Connected programs:", connectedPrograms)
+	return ""
 }
