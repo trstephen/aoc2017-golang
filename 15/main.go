49175b318acadf7c4db9b85253200279b8acc35c
Tyler Stephen <tyler.stephen@gmail.com>
2018-01-08 09:38:49 -0800

    Day 15, part 2

--- 15/main.go
+++ 15/main.go
@@ -6,29 +6,34 @@ import (
 )
 
 type generator struct {
-	val, factor int
+	val, factor, nextMultiple int
 }
 
-func newGenerator(initVal, factor int) *generator {
-	return &generator{initVal, factor}
+func newGenerator(initVal, factor, nextMultiple int) *generator {
+	return &generator{initVal, factor, nextMultiple}
 }
 
 func (g *generator) Next() {
 	g.val = (g.val * g.factor) % math.MaxInt32
+
+	if g.val%g.nextMultiple != 0 {
+		g.Next()
+	}
 }
 
 func main() {
-	// seedA := 65 // demo
 	seedA := 618
 	factorA := 16807
-	// seedB := 8921 // demo
+	multipleA := 4
+
 	seedB := 814
 	factorB := 48271
+	multipleB := 8
 
-	genA := newGenerator(seedA, factorA)
-	genB := newGenerator(seedB, factorB)
+	genA := newGenerator(seedA, factorA, multipleA)
+	genB := newGenerator(seedB, factorB, multipleB)
 
-	comparisonRounds := int(40e6)
+	comparisonRounds := int(5e6)
 	pairCount := 0
 
 	for i := 0; i < comparisonRounds; i++ {
